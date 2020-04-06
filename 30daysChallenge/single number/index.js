cbd3e63479a4a6887e5cdd1b34b6adb5d6902fe2
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-06 20:00:03 +0000

    moving numbers to the created bash

---
+++ 30daysChallenge/single number/index.js
@@ -0,0 +1,19 @@
+var singleNumber = function(nums) {
+  
+  var result = nums[0]
+
+  var memory1 = {};
+
+  var memory2 = {};
+
+  for(var i=0; i<nums.length; i++) {
+    
+    if(memory1[nums[0]]) {
+
+      delete memory1[nums[i]]
+
+      
+    }
+
+  }
+}
